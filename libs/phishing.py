a78b4d5ec627a1b98ae9f23f220790d4b644e1ad
lostinth1cho <59854090+lostinth1cho@users.noreply.github.com>
2022-06-16 01:51:25 +0800

    also update link.txt

--- libs/phishing.py
+++ libs/phishing.py
@@ -1,6 +1,5 @@
-from matplotlib.pyplot import text
 import requests
-from os import getcwd
+import os
 
 # clean ..\list.txt before running program
 
@@ -16,5 +15,18 @@ class Phishing:
         url = 'https://raw.githubusercontent.com/Dogino/Discord-Phishing-URLs/main/scam-urls.txt'
         r = requests.get(url)
         Phishing.tiananmenSquareList = r.text.split('\n')
-        ##print(Phishing.tiananmenSquareList[-1])
+        print('Successfully fetched latest phishing links at ' + url)
 
+        directory = os.path.dirname(__file__)
+        blackListFilePath2 = os.path.join(directory, 'link.txt')
+        f = open(blackListFilePath2, 'r', encoding = 'utf-8')
+        origin_TAMSlist = f.read().split('\n')
+        f.close()
+
+        TAMS_set1 = set(Phishing.tiananmenSquareList + origin_TAMSlist)
+        TAMS_list = list(TAMS_set1)
+        TAMS_list.sort()
+        TAMS_str = '\n'.join(TAMS_list)
+        f1 = open(blackListFilePath2, 'w', encoding = 'utf-8')
+        f1.write(TAMS_str)
+        print('Successfully updated latest phishing links in ' + blackListFilePath2)
